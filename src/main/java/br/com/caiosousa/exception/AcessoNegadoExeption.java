d8cbaacd0a56f12a40077c091372cf511a86cce5
Caio Pereira <caio.psw@gmail.com>
2013-10-22 21:02:18 -0200

    Adição do utilitário MD5

---
+++ src/main/java/br/com/caiosousa/exception/AcessoNegadoExeption.java
@@ -0,0 +1,6 @@
+package br.com.caiosousa.exception;
+
+public class AcessoNegadoExeption extends Exception {
+	private static final long serialVersionUID = 1L;
+	public static final AcessoNegadoExeption INSTANCE = new AcessoNegadoExeption();
+}
